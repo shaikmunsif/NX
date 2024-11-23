425cf7b59899566364e5f93934b50767c68327c6
shaikmunsif <shaikmunsif4@gmail.com>
2024-11-23 21:19:18 +0530

    initial workspace setup, installed NGRX, material

--- apps/NGRxPractice/src/app/app.config.ts
+++ apps/NGRxPractice/src/app/app.config.ts
@@ -1,10 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore(),
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideAnimationsAsync('noop'),
   ],
 };
